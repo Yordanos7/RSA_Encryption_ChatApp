0274abf9978e254748a0165d11e1e42023596abd
yordanos <154667985+Yordanos7@users.noreply.github.com>
2025-05-22 21:04:03 +0300

    Update App.jsx

--- frontend/rsa/src/App.jsx
+++ frontend/rsa/src/App.jsx
@@ -4,7 +4,7 @@ import forge from "node-forge";
 import MessageInput from "./components/MessageInput";
 import MessageDisplay from "./components/MessageDisplay";
 
-const socket = io("https://rsa-encryption-chatapp-3backend.onrender.com");
+const socket = io("https://rsa-encryption-chatapp-7.onrender.com");
 
 function App() {
   const [publicKeyPem, setPublicKeyPem] = useState(null);
